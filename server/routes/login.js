aae338407fe3a7c41c29b32e0c120d69ae3fb189
Nick Tsai <nicktsai1026@gmail.com>
2017-09-20 21:55:34 +0800

    add common page counter and facebook share button

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const getLikePages = require('../likePages');
 
 module.exports = function (app, db) {
     
@@ -27,4 +28,88 @@ module.exports = function (app, db) {
 
     app.get('/auth/facebook',
         passport.authenticate('facebook'));
+
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    })
+
+    //call api save in database
+    app.get('/setLikePages', (req, res) => {
+        var userId = req.session.passport.user;
+        db.collection('users').findOne({ fbId: userId }, (err, item) => {
+            if (err) return console.log(err)
+            var accessToken = item.access_token;
+            var path = `https://graph.facebook.com/v2.10/${userId}/likes?fields=name,fan_count,category,about,created_time,link,picture&access_token=${accessToken}`;
+            getLikePages.pageDetails(path)
+                .then((details) => {
+                    //check user likes page (for existed users)
+                    var newPageIdOnly = details[1];
+                    if (item.likes) {
+                        var oldPageIdOnly = [];
+                        item.likes.forEach((val) => {
+                            oldPageIdOnly.push(val.id);
+                        })
+                        let a = new Set(oldPageIdOnly);
+                        let b = new Set(newPageIdOnly);
+                        // ab difference set 差集
+                        let differenceABSet = new Set([...a].filter(x => !b.has(x)));
+                        let differenceArr = Array.from(differenceABSet);
+
+                        differenceArr.forEach((value) => {
+                            db.collection('pagedetails').findOne({ id: value }, (err, item) => {
+                                var newFbUserId = item.fbUserId.filter(function (id) {
+                                    return id != userId;
+                                })
+                                db.collection('pagedetails').updateOne({ id: value }, { $set: { fbUserId: newFbUserId } }, (err, item) => {
+                                    if (err) return console.log(err)
+                                })
+                            })
+                        })
+                    }
+                    var pageObj = {};
+                    pageObj.likes = details[0];
+                    db.collection('users').updateOne({ fbId: userId }, { $set: pageObj }, (err, item) => {
+                        if (err) return console.log(err)
+                        res.redirect('/setPageDetails');
+                    })
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        })
+    })
+
+    app.get('/setPageDetails', (req, res) => {
+        var userId = req.session.passport.user;
+        db.collection('users').findOne({ fbId: userId }, (err, item) => {
+            if (err) return console.log(err)
+            var pageCounter = 0;
+            item.likes.forEach((val) => {
+                pageCounter++;
+                db.collection('pagedetails').findOne({ id: val.id }, (err, item) => {
+                    if (item == null) {
+                        val.fbUserId = [userId];
+                        db.collection('pagedetails').insert(val, (err, item) => {
+                            if (err) return console.log(err)
+                        })
+                    } else {
+                        //check fbUserId arr had this facebookId or not
+                        var judge = item.fbUserId.indexOf(userId);
+                        if (judge < 0) {
+                            item.fbUserId.push(userId);
+                            db.collection('pagedetails').updateOne({ id: val.id }, { $set: item }, (err, item) => {
+                                if (err) return console.log(err)
+                            })
+                        }
+                    }
+                })
+            })
+            db.collection('users').updateOne({ fbId: userId }, { $set: { totalPage: pageCounter } }, (err, item) => {
+                if (err) return console.log(err);
+            })
+        })
+        res.redirect('/home');
+    })
+
 }
